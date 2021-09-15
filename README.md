ffcd6f68f587e1a252c4ccc9253d6d9a5dfff035
Mersad Dela <developer78991@gmail.com>
2021-09-16 00:45:53 +0200

    ...

--- README.md
+++ README.md
@@ -27,9 +27,6 @@ Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app w
 
 Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
 
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
 
 ## Web & Mobile preview
 
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -12,7 +12,7 @@ export class ItemService {
     new Item(2, 'Kids toy', 'Old phone kids toy', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4mUeIrKQmAnD01foj6K6p78Pjxdu7pqT27g&usqp=CAU'),
     new Item(3, 'Cat', 'My favourite pet', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjATKMvP5zBGNRh8hR0LJpo5zgiEB-G-d2yw&usqp=CAU'),
     new Item(4, 'Nokia 3310', 'My first cellphone', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3qs1pRlmF15pwrp6Pp0tgUAa4KMp69uUNFA&usqp=CAU'),
-    new Item(5, 'Tom & Jerry', 'The best cartoon fod kids', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROL-7HoB4xxFeidgloxFWkTRGxVlLFctFfig&usqp=CAU'),
+    new Item(5, 'Tom & Jerry', 'The best cartoon', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROL-7HoB4xxFeidgloxFWkTRGxVlLFctFfig&usqp=CAU'),
     new Item(6, 'Cevapi', 'Our favourite meal', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIZNH2hpp356Enj_zYRBSiOS9qLNU0Y7nLyQ&usqp=CAU'),
     new Item(7, 'Golf 2', 'Popular car in BiH', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSJeAbfQCJ3TCldutau0sRT1oFFKn9xEusqQ&usqp=CAU'),
     new Item(8, 'Trebevic', 'View from cable car', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7KUVVE7PUxK0CR0qOhlmFCSeLLc4Zm4Wgpg&usqp=CAU'),
